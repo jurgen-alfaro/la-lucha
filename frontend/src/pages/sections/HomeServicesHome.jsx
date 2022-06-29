34772d7e9680aeb0ab0a2792bcfc57f604cfd2ff
jurgen-alfaro <dev.jurgen.alfaro@gmail.com>
2022-06-28 23:32:17 -0600

    increased gap in mobile screens

--- frontend/src/pages/sections/HomeServicesHome.jsx
+++ frontend/src/pages/sections/HomeServicesHome.jsx
@@ -11,7 +11,7 @@ function HomeServicesHome() {
               Nuestros Servicios
             </h2>
 
-            <div className='service-items my-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-8'>
+            <div className='service-items my-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-3 mt-8'>
               <div className='service-item cursor-pointer hover:shadow-xl transition  duration-300 ease-in-out p-2 flex justify-center align-center flex-col text-center'>
                 <NavLink to='servicios' className='flex justify-center'>
                   <FaReceipt className='text-white lg:text-9xl md:text-8xl sm:text-7xl text-6xl' />
